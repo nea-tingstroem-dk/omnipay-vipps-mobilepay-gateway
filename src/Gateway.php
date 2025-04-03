adaede19b326968c0b6743e5fdfb11e3f16c54f4
nea-tingstroem-dk <55084026+nea-tingstroem-dk@users.noreply.github.com>
2025-04-03 10:55:19 +0200

    Use Dummy gateway as skeleton

--- src/Gateway.php
+++ src/Gateway.php
@@ -1,137 +1,147 @@
 <?php
-namespace Omnipay\VipsMobilepay;
+
+namespace Omnipay\VippsMobilepay;
 
 use Omnipay\Common\AbstractGateway;
+use Omnipay\VippsMobilepay\Message\AuthorizeRequest;
 
 /**
- * Description of VippsMobilepay
+ * VippsMobilepay Gateway
  *
- * @author niels
+ * This gateway is useful for testing. It simply authorizes any payment made using a valid
+ * credit card number and expiry.
+ *
+ * Any card number which passes the Luhn algorithm and ends in an even number is authorized,
+ * for example: 4242424242424242
+ *
+ * Any card number which passes the Luhn algorithm and ends in an odd number is declined,
+ * for example: 4111111111111111
  */
-class Gateway extends AbstractGateway{
+class Gateway extends AbstractGateway
+{
 
-
-    /**
-     * Get the processor name.
-     *
-     * @return string
-     */
-    public function getName():string {
+	   
+							  
+	  
+					 
+	   
+    public function getName()
+    {
         return 'VippsMobilepay';
     }
 
-    /**
-     * Declare the parameters that will be used to authenticate with the site.
-     *
-     * A getter (e.g getUsername for username) is required for each of these.
-     *
-     * @return array
-     */
+	   
+																			  
+	  
+																			 
+	  
+					
+	   
     public function getDefaultParameters()
     {
-        return array(
-          'merchant_id' => '',
-          'secret_key' => '',
-          'testMode' => false,
-          'key_version' => 2,
-        );
+        return array();
+							  
+							 
+							  
+							 
+		  
     }
 
-    /**
-     * Authorize credentials.
-     *
-     * @param array $parameters
-     *
-     * @return \Omnipay\Mercanet\Message\OffsiteAuthorizeRequest
-     */
+	   
+							 
+	  
+							   
+	  
+																
+	   
     public function authorize(array $parameters = array())
     {
-        return $this->createRequest('\Omnipay\Mercanet\Message\OffsiteAuthorizeRequest', $parameters);
+        return $this->createRequest('\Omnipay\VippsMobilepay\Message\AuthorizeRequest', $parameters);
     }
 
-    /**
-     *
-     * @param array $parameters
-     * @return \Omnipay\Mercanet\Message\OffsiteCaptureRequest
-     */
-    public function capture(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Mercanet\Message\OffsiteCaptureRequest', $parameters);
-    }
+	   
+	  
+							   
+															  
+	   
+														
+	 
+																									
+	 
 
-    /**
-     *
-     * @param array $parameters
-     * @return \Omnipay\Mercanet\Message\OffsitePurchaseRequest
-     */
+	   
+	  
+							   
+															   
+	   
     public function purchase(array $parameters = array())
     {
-        return $this->createRequest('\Omnipay\Mercanet\Message\OffsitePurchaseRequest', $parameters);
+        return $this->authorize($parameters);
+	 
+
+	   
+	  
+							   
+																	   
+	   
+																 
+	 
+																											 
+	 
+
+	   
+							   
+																 
+	   
+																  
+	 
+																											  
+	 
+
+	   
+							   
+																 
+	   
+																   
+	 
+																								   
+	 
+
+								   
+	 
+												  
+	 
+
+										 
+	 
+														  
+	 
+								  
+	 
+												 
+	 
+
+										
+	 
+														 
+	 
+
+	   
+				  
+	   
+								   
+	 
+												  
     }
 
-    /**
-     *
-     * @param array $parameters
-     * @return \Omnipay\Mercanet\Message\OffsiteCompletePurchaseRequest
-     */
-    public function completePurchase(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Mercanet\Message\OffsiteCompletePurchaseRequest', $parameters);
-    }
-
-    /**
-     * @param array $parameters
-     * @return \Omnipay\Mercanet\Message\CompleteAuthorizeRequest
-     */
-    public function completeAuthorize(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Mercanet\Message\OffsiteCompleteAuthorizeRequest', $parameters);
-    }
-
-    /**
-     * @param array $parameters
-     * @return \Omnipay\Mercanet\Message\CompleteAuthorizeRequest
-     */
-    public function acceptNotification(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Mercanet\Message\OffsiteNotifyRequest', $parameters);
-    }
-
-    public function getMerchantID()
-    {
-        return $this->getParameter('merchant_id');
-    }
-
-    public function setMerchantID($value)
-    {
-        return $this->setParameter('merchant_id', $value);
-    }
-    public function getSecretKey()
-    {
-        return $this->getParameter('secret_key');
-    }
-
-    public function setSecretKey($value)
-    {
-        return $this->setParameter('secret_key', $value);
-    }
-
-    /**
-     * @return int
-     */
-    public function getKeyVersion()
-    {
-        return $this->getParameter('key_version');
-    }
-
-    /**
-     * @param string $value
-     *
-     * @return OffsiteGateway
-     */
-    public function setKeyVersion($value)
-    {
-        return $this->setParameter('key_version', $value);
-    }
+	   
+						   
+	  
+							 
+	   
+										 
+	 
+														  
+	 
 
 }
